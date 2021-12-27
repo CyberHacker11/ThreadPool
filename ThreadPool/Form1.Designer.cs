b5f44bf91fba558cc0ef24c4a7ad3d63a0eb20ee
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-12-27 22:07:29 +0400

    Добавьте файлы проекта.

---
+++ ThreadPool/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ThreadPool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbxBrowse = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.rbtnEncrypt = new System.Windows.Forms.RadioButton();
+            this.rbtnDecrypt = new System.Windows.Forms.RadioButton();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.AutoSize = true;
+            this.btnBrowse.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBrowse.Location = new System.Drawing.Point(12, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(85, 29);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbxBrowse
+            // 
+            this.tbxBrowse.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbxBrowse.Location = new System.Drawing.Point(114, 13);
+            this.tbxBrowse.Multiline = true;
+            this.tbxBrowse.Name = "tbxBrowse";
+            this.tbxBrowse.Size = new System.Drawing.Size(462, 29);
+            this.tbxBrowse.TabIndex = 1;
+            this.tbxBrowse.TextChanged += new System.EventHandler(this.tbxBrowse_TextChanged);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPassword.Location = new System.Drawing.Point(18, 60);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(74, 19);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbxPassword.Location = new System.Drawing.Point(114, 55);
+            this.tbxPassword.Multiline = true;
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(138, 29);
+            this.tbxPassword.TabIndex = 3;
+            this.tbxPassword.TextChanged += new System.EventHandler(this.tbxPassword_TextChanged);
+            // 
+            // rbtnEncrypt
+            // 
+            this.rbtnEncrypt.AutoSize = true;
+            this.rbtnEncrypt.Checked = true;
+            this.rbtnEncrypt.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbtnEncrypt.Location = new System.Drawing.Point(331, 58);
+            this.rbtnEncrypt.Name = "rbtnEncrypt";
+            this.rbtnEncrypt.Size = new System.Drawing.Size(80, 23);
+            this.rbtnEncrypt.TabIndex = 4;
+            this.rbtnEncrypt.TabStop = true;
+            this.rbtnEncrypt.Text = "Encrypt";
+            this.rbtnEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // rbtnDecrypt
+            // 
+            this.rbtnDecrypt.AutoSize = true;
+            this.rbtnDecrypt.Enabled = false;
+            this.rbtnDecrypt.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rbtnDecrypt.Location = new System.Drawing.Point(457, 58);
+            this.rbtnDecrypt.Name = "rbtnDecrypt";
+            this.rbtnDecrypt.Size = new System.Drawing.Size(81, 23);
+            this.rbtnDecrypt.TabIndex = 5;
+            this.rbtnDecrypt.Text = "Decrypt";
+            this.rbtnDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 103);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(564, 30);
+            this.progressBar.TabIndex = 6;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.AutoSize = true;
+            this.btnCancel.Enabled = false;
+            this.btnCancel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.Location = new System.Drawing.Point(491, 150);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(85, 29);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.AutoSize = true;
+            this.btnStart.Enabled = false;
+            this.btnStart.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.Location = new System.Drawing.Point(390, 150);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 29);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 192);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.rbtnDecrypt);
+            this.Controls.Add(this.rbtnEncrypt);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.tbxBrowse);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbxBrowse;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.RadioButton rbtnEncrypt;
+        private System.Windows.Forms.RadioButton rbtnDecrypt;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ ThreadPool/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadPool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Open = new OpenFileDialog();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if(Open.ShowDialog() == DialogResult.OK)
+            {
+                tbxBrowse.Text = Open.FileName;
+            }
+            if (tbxPassword.Text.Length > 2 & tbxBrowse.Text != "") btnStart.Enabled = true;
+            else btnStart.Enabled = false;
+        }
+
+        OpenFileDialog Open { get; set; }
+        Timer Timer { get; set; }
+
+        private void tbxPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxPassword.Text.Length > 2 & tbxBrowse.Text != "") btnStart.Enabled = true;
+            else btnStart.Enabled = false;
+        }
+
+        private void tbxBrowse_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxPassword.Text.Length > 2 & tbxBrowse.Text != "") btnStart.Enabled = true;
+            else btnStart.Enabled = false;
+        }
+
+        private void Progress(object sender, EventArgs e)
+        {
+            if (progressBar.Value < 100) progressBar.Value++;
+            else
+            {
+                Timer.Stop();
+                progressBar.Value = 0;
+                MessageBox.Show("File Copied Successfully");
+            }
+        }
+
+        private void Tick()
+        {
+            Timer = new Timer();
+            Timer.Start();
+            Timer.Enabled = true;
+            Timer.Interval = 1000;
+            progressBar.Maximum = 100;
+            Timer.Tick += new EventHandler(Progress);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            rbtnEncrypt.Enabled = false;
+            rbtnDecrypt.Enabled = true;
+            rbtnDecrypt.Checked = true;
+            btnStart.Enabled = false;
+            btnCancel.Enabled = true;
+            Tick();
+        }
+    }
+}
