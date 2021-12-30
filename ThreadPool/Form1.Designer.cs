203fdd6a6017ccb0b3a42af616e86ac08e57e4be
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-12-30 18:36:56 +0400

    Update

--- ThreadPool/Form1.Designer.cs
+++ ThreadPool/Form1.Designer.cs
@@ -124,6 +124,7 @@
             this.btnCancel.TabIndex = 7;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnStart
             // 
--- ThreadPool/Form1.cs
+++ ThreadPool/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,20 +17,19 @@ namespace ThreadPool
         {
             InitializeComponent();
             Open = new OpenFileDialog();
+            Timer = new System.Windows.Forms.Timer();
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
             if(Open.ShowDialog() == DialogResult.OK)
             {
+                FileText = File.ReadAllText(Open.FileName);
                 tbxBrowse.Text = Open.FileName;
             }
             if (tbxPassword.Text.Length > 2 & tbxBrowse.Text != "") btnStart.Enabled = true;
             else btnStart.Enabled = false;
         }
-
-        OpenFileDialog Open { get; set; }
-        Timer Timer { get; set; }
 
         private void tbxPassword_TextChanged(object sender, EventArgs e)
         {
@@ -45,33 +45,80 @@ namespace ThreadPool
 
         private void Progress(object sender, EventArgs e)
         {
-            if (progressBar.Value < 100) progressBar.Value++;
+            if (progressBar.Value < 3) progressBar.Value++;
             else
             {
                 Timer.Stop();
                 progressBar.Value = 0;
-                MessageBox.Show("File Copied Successfully");
+
+
+                btnCancel.Enabled = false;
+                EncryptingAndDecrypting();
             }
         }
 
         private void Tick()
         {
-            Timer = new Timer();
             Timer.Start();
             Timer.Enabled = true;
             Timer.Interval = 1000;
-            progressBar.Maximum = 100;
+            progressBar.Maximum = 3;
             Timer.Tick += new EventHandler(Progress);
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            rbtnEncrypt.Enabled = false;
-            rbtnDecrypt.Enabled = true;
-            rbtnDecrypt.Checked = true;
             btnStart.Enabled = false;
             btnCancel.Enabled = true;
             Tick();
+        }
+
+        private void EncryptingAndDecrypting()
+        {
+            StringBuilder sb = new StringBuilder();
+            int len = int.Parse(tbxPassword.Text);
+            if (rbtnEncrypt.Enabled)
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    sb.Append((char)(FileText[i] ^ 2));
+                }
+                for (int i = len; i < FileText.Length; i++)
+                {
+                    sb.Append(FileText[i]);
+                }
+                FileText = sb.ToString();
+                MessageBox.Show("Text Sucessfully Encrypted!");
+                File.WriteAllText(Open.FileName, FileText);
+                
+                btnStart.Enabled = rbtnDecrypt.Enabled = rbtnDecrypt.Checked = true;
+                tbxPassword.Enabled = rbtnEncrypt.Enabled = false;
+            }
+            else if (rbtnDecrypt.Enabled)
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    sb.Append((char)(FileText[i] ^ 2));
+                }
+                for (int i = len; i < FileText.Length; i++)
+                {
+                    sb.Append(FileText[i]);
+                }
+                FileText = sb.ToString();
+                MessageBox.Show("Text Sucessfully Decrypting!");
+                File.WriteAllText(Open.FileName, FileText);
+            }
+        }
+
+        OpenFileDialog Open { get; set; }
+        string FileText { get; set; }
+        System.Windows.Forms.Timer Timer { get; set; }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+            progressBar.Value = 0;
+            btnCancel.Enabled = btnStart.Enabled = tbxPassword.Enabled = true;
         }
     }
 }
